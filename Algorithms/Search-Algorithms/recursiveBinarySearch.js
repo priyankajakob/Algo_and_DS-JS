47b8e1d0bdd98536464e3a5e9356fbd946a20e7e
Priyanka Jacob <priyanka.jacob93@gmail.com>
2022-09-23 13:03:26 +0530

    fix : recursiveBinarySearch

--- Algorithms/Search-Algorithms/recursiveBinarySearch.js
+++ Algorithms/Search-Algorithms/recursiveBinarySearch.js
@@ -1,7 +1,29 @@
+function recursiveCheck (arr,left,right,target) {
+
+  //Base case
+  if (left>right) {
+    return -1
+  }
+  
+  let mid = Math.floor((left+right)/2)
+
+  if (arr[mid]==target) {
+    return mid
+  } 
+
+  else if (target < arr[mid]) {
+    return recursiveCheck(arr,left,mid,target)
+  }
+
+  else if (target > arr[mid]) {
+    return recursiveCheck(arr,mid+1,right,target)
+  }
+
+}
+
 
 function recursiveBinarySearch(arr,target) { 
 
-  //Base case
   if (arr.length == 0 ) {
     return -1
   }
@@ -11,25 +33,14 @@ function recursiveBinarySearch(arr,target) {
 
   let left = 0
   let right = arr.length - 1
-  let mid = Math.floor((left+right)/2) 
 
-//   console.log(left, right, mid, arr[mid],target)
-  if (arr[mid]==target) {
-    return mid
-  } 
+  return recursiveCheck(arr,left,right,target)
 
-  else if (target < arr[mid]) {
-    return recursiveBinarySearch(arr.slice(left,mid),target)
-  }
-
-  else if (target > arr[mid]) {
-    return recursiveBinarySearch(arr.slice(mid+1,right),target)
-  }
 }
 
-// console.log(recursiveBinarySearch([2],2))
+console.log(recursiveBinarySearch([2],2))
 console.log(recursiveBinarySearch([2,3,4,5,6,7,10,23,31],4))
-// console.log(recursiveBinarySearch([2,3,4,5,6,7,10,23,31],10))
-// console.log(recursiveBinarySearch([2,3,4,5,6,7,10,23,31],31))
-// console.log(recursiveBinarySearch([2,3,4,5,6,7,10,23,31],2))
+console.log(recursiveBinarySearch([2,3,4,5,6,7,10,23,31],10))
+console.log(recursiveBinarySearch([2,3,4,5,6,7,10,23,31],31))
+console.log(recursiveBinarySearch([2,3,4,5,6,7,10,23,31],2))
 
